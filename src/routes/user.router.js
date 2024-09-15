6a50a506625fea20d762ba1edb76659eabb9b172
ryzen <iron4540@gmail.com>
2024-09-15 21:35:19 +0530

    add routes  to routers file

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import upload from "../middlewares/multer.middleeware.js"; // Adjust based on your export
-import { registerUser , loginUser, logoutUser  , ChangePassword ,ChangeProfilePhoto , getCurrentUser, ChangeCoverImage ,up} from "../controllers/user.controller.js";
+import { registerUser , loginUser, logoutUser  , ChangePassword ,ChangeProfilePhoto , getCurrentUser, ChangeCoverImage , getUserChannelProfile, UserHistory, updateUserData} from "../controllers/user.controller.js";
 import { verifyJWT} from "../middlewares/Auth.middleware.js";
-import jwt from "jsonwebtoken";
+
 
 const router = Router();
 
@@ -23,5 +23,8 @@ router.route("/register")
   router.route("/ch-avatar").patch(verifyJWT ,upload.single("avatar"),ChangeProfilePhoto);
   router.route("/profile").get(getCurrentUser);
   router.route("/ch-cover-img").patch(verifyJWT ,upload.single("coverImage") , ChangeCoverImage);
+  router.route("/c/:username").get(verifyJWT , getUserChannelProfile );
+  router.route("/history").get(verifyJWT , UserHistory);
+  router.route("/up-userdata").post(verifyJWT , updateUserData);
 
 export  default router;
