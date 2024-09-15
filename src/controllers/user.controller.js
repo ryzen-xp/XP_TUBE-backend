a4b65ebc68efb13edeeb5a84ad856b0d5c4064de
ryzen <iron4540@gmail.com>
2024-09-15 20:30:36 +0530

    watch history controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -295,5 +295,121 @@ const updateUserData = asyncHandler( async( req ,res)=>{
   return   res.status(200).json(ApiResponse( 200 ,user,  "Update User data successfully !!"));
 });
 
+//  To make  video  conroller  in this api 
+const getUserChannelProfile = asyncHandler( async (req , res)=>{
+   const {username} = req.params ;
+   if(!username?.trim()){
+    throw new ApiError(400 , "username missing")
+   }
 
-export { registerUser, loginUser , logoutUser ,refreshAccessToken , ChangePassword , getCurrentUser ,ChangeProfilePhoto ,ChangeCoverImage , updateUserData};
+  //  const user = await User.find({username});
+
+   const channel  =  await User.aggregate([{
+       $match :{
+        username : username?.toLowerCase()
+       }},
+        {
+          $lookup :{
+                from : "subscriptions" ,
+                localField : "_id" ,
+                foreignField : "channel" ,
+                as: "subscribers"
+          }
+
+       }
+       ,{
+        $lookup :{
+          from : "subscription" ,
+          localField : "_id" ,
+          foreignField : "subscriber" ,
+          as : "subscribedTo"
+        }
+       } ,
+       {
+        $addFields: {
+          subscribersCount : {
+            $size: "$subscribers"
+          } ,
+          channelsSubscribedCount : {
+            $size: "$subscribedTo"
+          }
+          ,
+          
+            isSubscribed : {
+              $cond : {
+                if : {$in: [req.user?._id , "subscribers.subscriber"]} ,
+                then : true  ,
+                else : false 
+              }
+            }
+          
+        }
+       } ,{
+        $project : {
+          fullName : 1 ,
+          username : 1 ,
+          subscribersCount : 1 , 
+          channelsSubscribedCount  : 1 ,
+          isSubscribed :  1 ,
+          avatar : 1 ,
+          coverImage : 1 ,
+          email : 1
+        }
+       }
+   ])
+
+   if(!channel?.length){
+    throw  new ApiError( 404 ," channel does not exists")
+
+   }
+
+    return res.status(200).json( new ApiResponse(200 , " User Channel  fetched  successfully "));
+
+}) ;
+
+//  History  finding  in this  function   
+  const  UserHistory  =  asyncHandler ( async ( req, res)=> {
+       const user =  await  User.aggregate([
+        {
+          $match : {
+            _id: new mongoose.Types.ObjectId(req.user._id)
+          },
+          $lookup: {
+            from: "videos" ,
+            localField : "watchHistory" ,
+            foreignField : "_id",
+            as : "watchHistory",
+            pipeline: [{
+              $lookup:{
+                from:"users" ,
+                localField :"owner" ,
+                foreignField : "_id" ,
+                as : "owner" ,
+                pipeline: [{
+                  $project : {
+                    fullName : 1  ,
+                    username: 1,
+                    avatar : 1
+                  }
+                }]
+              }
+            },
+          {
+            $addFields : {
+              owner:{
+                $first : "$owner"
+              }
+            }
+          }]
+          }
+        }
+       ]);
+        return  res.status(200).json( new ApiResponse (200 ,  user[0].UserHistory , "watch History fetch  successfully "));
+
+  });
+
+
+export { registerUser, loginUser , logoutUser ,refreshAccessToken , ChangePassword , getCurrentUser ,ChangeProfilePhoto ,ChangeCoverImage , updateUserData ,getUserChannelProfile , UserHistory};
+
+
+// fat when measure wish access execute picnic clutch isolate arctic post artefact
